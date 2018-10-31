d74e282b7a9d3059952bfdb82a1f6e72ecefdd53
nenadmilutinovic <44362916+nenadmilutinovic@users.noreply.github.com>
2018-10-31 13:06:19 +0100

    Revert "Revert "Revert "Revert "File architecture update""""
    This reverts commit 6a4651ce93497caba4f908ecc00815cf0c1183b3.

--- runestone_petlja_ext/pygamelib/js/pygamelib/display.js
+++ runestone_petlja_ext/pygamelib/js/pygamelib/display.js
@@ -1,1 +1,20 @@
-var $builtinmodule = PygameLib.display_module;
+//pygame display
+var $builtinmodule = function (name) {
+    var mod = {};
+    mod.set_mode = new Sk.builtin.func(function (size) {
+        mod.surface = Sk.misceval.callsim(PygameLib.SurfaceType, size);
+        PygameLib.surface = mod.surface;
+        return mod.surface;
+    });
+    mod.update = new Sk.builtin.func(function() {
+        Sk.misceval.callsim(mod.surface.update, mod.surface);
+    });
+    mod.flip = new Sk.builtin.func(function() {
+        Sk.misceval.callsim(mod.surface.update, mod.surface);
+    });
+    mod.set_caption = new Sk.builtin.func(function(caption) {
+        if ($('.modal-title')) $('.modal-title').html(Sk.ffi.remapToJs(caption));
+        PygameLib.caption = Sk.ffi.remapToJs(caption);
+    });
+    return mod;
+};
